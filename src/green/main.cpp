9d163359856273e905f69676e50699a2d7e9c5b6
Ben Allen <benos.9k@gmail.com>
2020-06-15 15:20:38 +1200

    add git describe to window title

--- src/green/main.cpp
+++ src/green/main.cpp
@@ -734,7 +734,7 @@ int main() {
 	// request a window that can perform gamma correction
 	glfwWindowHint(GLFW_SRGB_CAPABLE, true);
 
-	window = glfwCreateWindow(1280, 800, "Multi-Resolution Subsampled Saliency", nullptr, nullptr);
+	window = glfwCreateWindow(1280, 800, (std::string("Multi-Resolution Subsampled Saliency (") + git_describe() + ")").c_str(), nullptr, nullptr);
 	if (!window) {
 		cerr << "Error: Could not create GLFW window" << endl;
 		abort();
