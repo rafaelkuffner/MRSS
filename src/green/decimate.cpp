0baffc4964d4fd672cb82dcebb748b45cb730197
Ben Allen <benos.9k@gmail.com>
2021-06-16 15:39:58 +1200

    refactor decimation computation

--- src/green/decimate.cpp
+++ src/green/decimate.cpp
@@ -273,6 +273,175 @@ namespace {
 		bool use_bins = false;
 	};
 
+	class Decimation {
+	private:
+		decimate_mesh_params m_mparams;
+		decimate_user_params m_uparams;
+		decimate_progress &m_progress;
+		OpenMesh::Decimater::DecimaterT<PolyMesh> m_decimater;
+		ModProgress<PolyMesh>::Handle m_hmod_progress;
+		ModSeamPreservation<PolyMesh>::Handle m_hmod_seams;
+		std::chrono::steady_clock::time_point m_time_start = std::chrono::steady_clock::now();
+		int m_target_collapses = 0;
+
+	public:
+		Decimation(const decimate_mesh_params &mparams, const decimate_user_params &uparams, decimate_progress &progress) :
+			m_mparams(mparams),
+			m_uparams(uparams),
+			m_progress(progress),
+			m_decimater(*mparams.mesh)
+		{
+			// progress reporting module
+			m_decimater.add(m_hmod_progress);
+			auto &mod_progress = m_decimater.module(m_hmod_progress);
+			mod_progress.progress = &progress;
+			mod_progress.time_start = m_time_start;
+			mod_progress.show_progress = uparams.show_progress;
+
+			// seam preservation module
+			m_decimater.add(m_hmod_seams);
+
+			if (uparams.use_tris) {
+				// NOTE needs triangulated mesh to work
+				const float verts_per_tri = float(mparams.mesh->n_vertices()) / float(mparams.mesh->n_faces());
+				m_uparams.targetverts = int(uparams.targettris * verts_per_tri);
+				std::cout << "decimating to approximately " << uparams.targettris << " triangles" << std::endl;
+			}
+
+			std::cout << "decimating to " << m_uparams.targetverts << " vertices" << std::endl;
+		}
+
+		bool run() {
+			if (m_uparams.targetverts >= m_mparams.mesh->n_vertices()) return true;
+
+			m_target_collapses = m_mparams.mesh->n_vertices() - m_uparams.targetverts;
+			m_progress.target_collapses = m_target_collapses;
+			const float target_ratio = float(m_uparams.targetverts) / m_mparams.mesh->n_vertices();
+			std::cout << "total vertices to remove: " << m_target_collapses << "; " << (1.f - target_ratio) << std::endl;
+
+			if (!run_binned()) {
+				m_progress.state = decimation_state::cancelled;
+				return false;
+			}
+
+			std::cout << "final vertices: " << m_mparams.mesh->n_vertices() << std::endl;
+			std::cout << "final triangles: " << m_mparams.mesh->n_faces() << std::endl;
+			m_progress.elapsed_time = std::chrono::duration_cast<decltype(decimate_progress::elapsed_time)>(std::chrono::steady_clock::now() - m_time_start);
+			std::cout << "decimate time: " << (m_progress.elapsed_time / std::chrono::duration<double>(1.0)) << "s" << std::endl;
+			bool cancelled = m_progress.should_cancel;
+			m_progress.state = cancelled ? decimation_state::cancelled : decimation_state::done;
+			return !cancelled;
+		}
+
+	private:
+		bool run_binned() {
+			auto &mod_progress = m_decimater.module(m_hmod_progress);
+
+			// vertex binning module
+			ModVertexBinning<PolyMesh>::Handle hmod_bins;
+			m_decimater.add(hmod_bins);
+			auto &mod_bins = m_decimater.module(hmod_bins);
+
+			// quadric error weighting module
+			OpenMesh::Decimater::ModQuadricT<PolyMesh>::Handle hmod_quadric;
+			m_decimater.add(hmod_quadric);
+			auto &mod_quadric = m_decimater.module(hmod_quadric);
+			mod_quadric.unset_max_err();
+
+			m_progress.state = decimation_state::bins;
+
+			if (m_uparams.use_saliency) {
+				mod_bins.use_bins = true;
+			} else {
+				std::cout << "decimating without saliency, bins will be ignored" << std::endl;
+			}
+
+			const int nbins = m_uparams.nbins;
+			const int bin_min_keep = m_uparams.targetverts * 0.1f / float(nbins);
+
+			std::vector<float> bin_weights(nbins, 0);
+			std::vector<int> bin_keep(nbins, 0);
+			float bin_weight_divisor = 0;
+			for (int i = 0; i < nbins; i++) {
+				// bin weight params: higher bin weight => more vertices _kept_ in that bin
+				// (saliency) weight: 0 (even weights across bins) .. 1 (least weight to low bin, most weight to high bin)
+				// power: non-linearity of weighting; 1 (linear) .. 2 (quadratic, more extreme)
+				const float d = m_uparams.weight;
+				const float x = (i + 0.5f) / float(nbins);
+				const float w = std::max(std::pow(x, m_uparams.power) * d + (1.f - d) * 0.5f, 0.f);
+				bin_weights[i] = w;
+				bin_weight_divisor += w;
+			}
+			for (int i = 0; i < nbins; i++) {
+				float w = bin_weights[i] /= bin_weight_divisor;
+				bin_keep[i] = std::max<int>(m_uparams.targetverts * w, bin_min_keep);
+			}
+
+			const auto sal_bin_edges = saliency_bin_edges(*m_mparams.mesh, m_mparams.prop_saliency, mod_bins.prop_bin, nbins);
+			const auto init_bin_counts = saliency_bin_counts(*m_mparams.mesh, mod_bins.prop_bin, nbins);
+
+			for (int i = 0; i < nbins; i++) {
+				int k = bin_keep[i];
+				int c = init_bin_counts[i];
+				std::cout << "bin " << i << ": keep " << k << "/" << c << " (" << bin_weights[i] << "); remove " << (c - k) << std::endl;
+				if (k > c) {
+					std::cout << "warning: can't keep more vertices than available, result will have fewer vertices than desired" << std::endl;
+				}
+			}
+
+			m_progress.state = decimation_state::run;
+			m_decimater.initialize();
+			for (int i = 0; i < nbins; i++) {
+				if (m_progress.should_cancel) return false;
+				std::cout << "decimating bin " << i << std::endl;
+				mod_bins.current_bin = i;
+				int target = init_bin_counts[i] - bin_keep[i];
+				int collapses = m_decimater.decimate_to(std::max<size_t>(m_mparams.mesh->n_vertices() - target, m_uparams.targetverts));
+				mod_progress.update();
+				std::cout << "\nvertices removed: " << collapses << std::endl;
+			}
+			m_mparams.mesh->garbage_collection();
+
+			const auto fin_bin_counts = saliency_bin_counts(*m_mparams.mesh, mod_bins.prop_bin, nbins);
+
+			for (int i = 0; i < nbins; i++) {
+				int k = fin_bin_counts[i];
+				int r = init_bin_counts[i] - k;
+				std::cout << "bin " << i << ": kept " << k << " (" << (float(k) / m_mparams.mesh->n_vertices())
+					<< "); removed " << r << " (" << (float(r) / m_target_collapses) << ")" << std::endl;
+			}
+
+			collect_errors(mod_quadric);
+
+			return true;
+
+		}
+
+		void collect_errors(OpenMesh::Decimater::ModQuadricT<PolyMesh> &mod_quadric) {
+			if (!m_mparams.prop_dec_error.is_valid()) return;
+			std::cout << "collecting decimation error values" << std::endl;
+			auto prop_v_err = m_mparams.prop_dec_error;
+			// init vertex errors
+			for (auto &e : m_mparams.mesh->property(prop_v_err).data_vector()) {
+				e = FLT_MAX;
+			}
+			// find smallest halfedge error for each vertex
+			for (auto hh : m_mparams.mesh->halfedges()) {
+				OpenMesh::Decimater::CollapseInfoT<PolyMesh> ci(*m_mparams.mesh, hh);
+				float h_err = mod_quadric.collapse_priority(ci);
+				float &v_err = m_mparams.mesh->property(prop_v_err, ci.v0);
+				v_err = std::min(v_err, h_err);
+			}
+			// deal with non-collapsibles
+			for (auto &e : m_mparams.mesh->property(prop_v_err).data_vector()) {
+				if (!(e < FLT_MAX)) {
+					// hackish?
+					e = 0;
+				}
+			}
+		}
+	};
+
 }
 
 namespace green {
@@ -286,138 +455,8 @@ namespace green {
 	}
 
 	bool decimate(const decimate_mesh_params &mparams, const decimate_user_params &uparams, decimate_progress &progress) {
-		progress.state = decimation_state::bins;
-		const auto time_start = std::chrono::steady_clock::now();
-
-		// NOTE needs triangulated mesh to work
-		const float verts_per_tri = float(mparams.mesh->n_vertices()) / float(mparams.mesh->n_faces());
-		const int targetverts = uparams.use_tris ? int(uparams.targettris * verts_per_tri) : uparams.targetverts;
-
-		if (uparams.use_tris) std::cout << "decimating to approximately " << uparams.targettris << " triangles" << std::endl;
-		std::cout << "decimating to " << targetverts << " vertices" << std::endl;
-
-		if (targetverts >= mparams.mesh->n_vertices()) return true;
-
-		OpenMesh::Decimater::DecimaterT<PolyMesh> decimater(*mparams.mesh);
-
-		// progress reporting module
-		ModProgress<PolyMesh>::Handle hmod_progress;
-		decimater.add(hmod_progress);
-		auto &mod_progress = decimater.module(hmod_progress);
-		mod_progress.progress = &progress;
-		mod_progress.time_start = time_start;
-		mod_progress.show_progress = uparams.show_progress;
-
-		// seam preservation module
-		ModSeamPreservation<PolyMesh>::Handle hmod_seams;
-		decimater.add(hmod_seams);
-
-		// vertex binning module
-		ModVertexBinning<PolyMesh>::Handle hmod_bins;
-		decimater.add(hmod_bins);
-		auto &mod_bins = decimater.module(hmod_bins);
-
-		// quadric error weighting module
-		OpenMesh::Decimater::ModQuadricT<PolyMesh>::Handle hmod_quadric;
-		decimater.add(hmod_quadric);
-		auto &mod_quadric = decimater.module(hmod_quadric);
-		mod_quadric.unset_max_err();
-
-		if (uparams.use_saliency) {
-			mod_bins.use_bins = true;
-		} else {
-			std::cout << "decimating without saliency, bins will be ignored" << std::endl;
-		}
-
-		const int nbins = uparams.nbins;
-		const int target_collapses = mparams.mesh->n_vertices() - targetverts;
-		progress.target_collapses = target_collapses;
-		const float target_ratio = float(targetverts) / mparams.mesh->n_vertices();
-		std::cout << "total vertices to remove: " << target_collapses << "; " << (1.f - target_ratio) << std::endl;
-
-		const int bin_min_keep = targetverts * 0.1f / float(nbins);
-
-		std::vector<float> bin_weights(nbins, 0);
-		std::vector<int> bin_keep(nbins, 0);
-		float bin_weight_divisor = 0;
-		for (int i = 0; i < nbins; i++) {
-			// bin weight params: higher bin weight => more vertices _kept_ in that bin
-			// (saliency) weight: 0 (even weights across bins) .. 1 (least weight to low bin, most weight to high bin)
-			// power: non-linearity of weighting; 1 (linear) .. 2 (quadratic, more extreme)
-			const float d = uparams.weight;
-			const float x = (i + 0.5f) / float(nbins);
-			const float w = std::max(std::pow(x, uparams.power) * d + (1.f - d) * 0.5f, 0.f);
-			bin_weights[i] = w;
-			bin_weight_divisor += w;
-		}
-		for (int i = 0; i < nbins; i++) {
-			float w = bin_weights[i] /= bin_weight_divisor;
-			bin_keep[i] = std::max<int>(targetverts * w, bin_min_keep);
-		}
-
-		const auto sal_bin_edges = saliency_bin_edges(*mparams.mesh, mparams.prop_saliency, mod_bins.prop_bin, nbins);
-		const auto init_bin_counts = saliency_bin_counts(*mparams.mesh, mod_bins.prop_bin, nbins);
-
-		for (int i = 0; i < nbins; i++) {
-			int k = bin_keep[i];
-			int c = init_bin_counts[i];
-			std::cout << "bin " << i << ": keep " << k << "/" << c << " (" << bin_weights[i] << "); remove " << (c - k) << std::endl;
-			if (k > c) {
-				std::cout << "warning: can't keep more vertices than available, result will have fewer vertices than desired" << std::endl;
-			}
-		}
-
-		progress.state = decimation_state::run;
-		decimater.initialize();
-		for (int i = 0; i < nbins; i++) {
-			if (progress.should_cancel) {
-				progress.state = decimation_state::cancelled;
-				return false;
-			}
-			std::cout << "decimating bin " << i << std::endl;
-			mod_bins.current_bin = i;
-			int target = init_bin_counts[i] - bin_keep[i];
-			int collapses = decimater.decimate_to(std::max<size_t>(mparams.mesh->n_vertices() - target, targetverts));
-			mod_progress.update();
-			std::cout << "\nvertices removed: " << collapses << std::endl;
-		}
-		mparams.mesh->garbage_collection();
-
-		const auto fin_bin_counts = saliency_bin_counts(*mparams.mesh, mod_bins.prop_bin, nbins);
-
-		for (int i = 0; i < nbins; i++) {
-			int k = fin_bin_counts[i];
-			int r = init_bin_counts[i] - k;
-			std::cout << "bin " << i << ": kept " << k << " (" << (float(k) / mparams.mesh->n_vertices()) << "); removed " << r << " (" << (float(r) / target_collapses) << ")" << std::endl;
-		}
-
-		std::cout << "collecting decimation error values" << std::endl;
-		auto prop_v_err = mparams.prop_dec_error;
-		// init vertex errors
-		for (auto &e : mparams.mesh->property(prop_v_err).data_vector()) {
-			e = FLT_MAX;
-		}
-		// find smallest halfedge error for each vertex
-		for (auto hh : mparams.mesh->halfedges()) {
-			OpenMesh::Decimater::CollapseInfoT<PolyMesh> ci(*mparams.mesh, hh);
-			float h_err = mod_quadric.collapse_priority(ci);
-			float &v_err = mparams.mesh->property(prop_v_err, ci.v0);
-			v_err = std::min(v_err, h_err);
-		}
-		// deal with non-collapsibles
-		for (auto &e : mparams.mesh->property(prop_v_err).data_vector()) {
-			if (!(e < FLT_MAX)) {
-				// hackish?
-				e = 0;
-			}
-		}
-
-		std::cout << "final vertices: " << mparams.mesh->n_vertices() << std::endl;
-		std::cout << "final triangles: " << mparams.mesh->n_faces() << std::endl;
-		progress.elapsed_time = std::chrono::duration_cast<decltype(decimate_progress::elapsed_time)>(std::chrono::steady_clock::now() - time_start);
-		std::cout << "decimate time: " << (progress.elapsed_time / std::chrono::duration<double>(1.0)) << "s" << std::endl;
-		progress.state = progress.should_cancel ? decimation_state::cancelled : decimation_state::done;
-		return !progress.should_cancel;
+		Decimation d(mparams, uparams, progress);
+		return d.run();
 	}
 
 }
